16b90f4ed38b279b3f016ae30992889518491c8c
Matthias <m@tth.es>
2019-06-04 22:17:47 +0200

    added cert store test

---
+++ tests/SecureCertTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matthias
+ * Date: 04.06.19
+ * Time: 22:13
+ */
+
+namespace Tests;
+
+
+use Phore\Core\Helper\PhoreSecretBoxSync;
+use Phore\Letsencrypt\PhoreCert;
+use Phore\Letsencrypt\PhoreSecureCertStore;
+use PHPUnit\Framework\TestCase;
+
+class SecureCertTest extends TestCase
+{
+
+
+    public function testEncryptCert()
+    {
+        $cert = new PhoreSecureCertStore("asdfasdfasdf", "/tmp");
+
+        $n = new PhoreCert();
+        $n->load(phore_file(__DIR__ . "/../demo_cert/data1.insecure.optools.net.json")->get_json());
+
+        $cert->addCert("abc", $n);
+
+        $c = $cert->getCertPem("abc");
+    }
+
+}
